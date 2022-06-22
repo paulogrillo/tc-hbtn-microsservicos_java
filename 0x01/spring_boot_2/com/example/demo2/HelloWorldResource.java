b11508718c596cc270721e5bfa67c19f78510a5b
Paulo Grillo <paulodigitalart@gmail.com>
2022-06-22 19:14:16 -0300

    Task 01 Initializr Spring

---
+++ 0x01/spring_boot_2/com/example/demo2/HelloWorldResource.java
@@ -0,0 +1,17 @@
+package com.example.demo2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/messages")
+public class HelloWorldResource {
+	
+	
+	@GetMapping("/home")
+	public String home() {
+		return "PROJETO MAVEN (SPRING INITIALIZR) CRIADO COM SUCESSO !!!";
+	}
+
+}
