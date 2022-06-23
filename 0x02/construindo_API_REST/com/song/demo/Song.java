95bbfe20b82c8f622c9433264e2502130737840f
Paulo Grillo <paulodigitalart@gmail.com>
2022-06-23 16:26:07 -0300

    Refactor class Song.java

--- 0x02/construindo_API_REST/com/song/demo/Song.java
+++ 0x02/construindo_API_REST/com/song/demo/Song.java
@@ -1,5 +1,7 @@
 package com.song.demo;
 
+import java.util.Objects;
+
 public class Song {
 	/*
 	 * - id: Integer 
@@ -12,17 +14,23 @@ public class Song {
 	private Integer id;
 	private String nome;
 	private String artista;
+	private String album;
 	private String anoLancamento;
+	
 	
 	public Song(){}
 	
-	public Song(Integer id, String nome, String artista, String anoLancamento) {
+
+
+	public Song(Integer id, String nome, String artista, String album, String anoLancamento) {
 		super();
 		this.id = id;
 		this.nome = nome;
 		this.artista = artista;
+		this.album = album;
 		this.anoLancamento = anoLancamento;
 	}
+
 
 	public Integer getId() {
 		return id;
@@ -56,37 +64,36 @@ public class Song {
 		this.anoLancamento = anoLancamento;
 	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
 
-        Song song = (Song) o;
 
-        if (id != null ? !id.equals(song.id) : song.id != null) return false;
-        if (nome != null ? !nome.equals(song.nome) : song.nome != null) return false;
-        if (artista != null ? !artista.equals(song.artista) : song.artista != null) return false;
-        return anoLancamento != null ? anoLancamento.equals(song.anoLancamento) : song.anoLancamento == null;
-    }
+	@Override
+	public int hashCode() {
+		return Objects.hash(album, anoLancamento, artista, id, nome);
+	}
 
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (nome != null ? nome.hashCode() : 0);
-        result = 31 * result + (artista != null ? artista.hashCode() : 0);
-        result = 31 * result + (anoLancamento != null ? anoLancamento.hashCode() : 0);
-        return result;
-    }
 
-    @Override
-    public String toString() {
-        return "Song{" +
-                "id=" + id +
-                ", nome='" + nome + '\'' +
-                ", artista='" + artista + '\'' +
-                ", anoLancamento='" + anoLancamento + '\'' +
-                '}';
-    }
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Song other = (Song) obj;
+		return Objects.equals(album, other.album) && Objects.equals(anoLancamento, other.anoLancamento)
+				&& Objects.equals(artista, other.artista) && Objects.equals(id, other.id)
+				&& Objects.equals(nome, other.nome);
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Song [id=" + id + ", nome=" + nome + ", artista=" + artista + ", album=" + album + ", anoLancamento="
+				+ anoLancamento + "]";
+	}
 
 	
 }
